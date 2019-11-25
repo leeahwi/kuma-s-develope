a00bc91e24b0d0c032ae4efb16b17114bb7f9b49
leeahwi <48510004+leeahwi@users.noreply.github.com>
2019-11-25 19:30:41 +0900

    Update animalbot.py

--- animalbot.py
+++ animalbot.py
@@ -12,13 +12,17 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print("ㅡㅡㅡㅡㅡㅡ")
-    await bot.change_presence(game=discord.Game(name='공부중', type=1))
+    game = discord.Game("군생활")
+    await bot.change_presence(activity=game, status=None)
 
 @bot.event
 async def on_message(message):
-    if message.content.startswith("!안녕"):
-        await bot.send_message(message.channel, "안녕하세요")
-
-
+ 
+    if message.author == client.user:
+        return
+ 
+    if message.content.startswith('hi'):
+        await bot.send_message(message.channel, embed=discord.Embed(description="hi"))
+        
 access_token = os.environ['BOT_TOKEN']
 bot.run(access_token)
