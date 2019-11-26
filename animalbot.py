a18a0c00d8e9310996e2f932325b59c4c0c625a6
leeahwi <48510004+leeahwi@users.noreply.github.com>
2019-11-26 20:18:51 +0900

    Update animalbot.py

--- animalbot.py
+++ animalbot.py
@@ -17,92 +17,87 @@ async def on_ready():
     print('Username: ' + str(client.user.name))
     print('Client ID: ' + str(client.user.id))
     print('Invite URL: ' + 'https://discordapp.com/oauth2/authorize?&client_id=' + client.user.id + '&scope=bot&permissions=0')
+	
+# Announce the change in voice state through text to speech (ignores mutes/deafens)
+@client.event
+async def on_voice_state_update(before, after):
+    # Ensure bot is connected to voice client (!join has been used)
+    if client.is_voice_connected(before.server) == True:
+        global player #모든 유저에 해당하는 구절인가?
+        previousChannel = before.voice_channel
+        newChannel = after.voice_channel
 
+        # Bot only talks when user's channel changes, not on mutes/deafens
+        if previousChannel != newChannel:
+            # When user joins or moves to bot's channel 유저가 봇이 있는 채널에서 나가거나 들어올때
+            if newChannel == currentChannel:
+                tts.createAnnouncement(after.name, '이(가) 들어왔어요')
+		#features/tts.py
+            # When user leaves bot's channel
+            elif previousChannel != None and newChannel == None and previousChannel == currentChannel:
+                tts.createAnnouncement(after.name, '이(가) 나갔어요')
+		
+            # When user moves out of bot's channel to a new channel
+            elif previousChannel == currentChannel and newChannel != currentChannel:
+                tts.createAnnouncement(after.name, '이(가) 다른 채널로 이동했어요')
+
+            # After user joins, leaves or moves, announce the new announcement
+            if (newChannel == currentChannel or previousChannel != None and
+                newChannel == None and previousChannel == currentChannel or
+                previousChannel == currentChannel and newChannel != currentChannel):
+
+                try:
+                    if player.is_playing() == False:
+                        player = voice.create_ffmpeg_player('announce.mp3')
+                        player.start()
+
+                except NameError:
+                    player = voice.create_ffmpeg_player('announce.mp3')
+                    player.start()
+
+			
 @client.event
 async def on_message(message):
+    # If the message author isn't the bot and the message starts with the
+    # command prefix ('!' by default), check if command was executed
+    if message.author.id != config.BOTID and message.content.startswith(config.COMMANDPREFIX):
+	# 봇의 메세지가 명령으로 인식 안되게 함
+        # Remove prefix and change to lowercase so commands aren't case-sensitive
+        message.content = message.content[1:].lower()
+          #conmmandprefix 다음 내용을 message.content로 받음
+        # Shuts the bot down - only usable by the bot owner specified in config
+        if message.content.startswith('shutdown') and message.author.id == config.OWNERID:
+            await client.send_message(message.channel, '전 이제 잘게요~ 쿰나잇')
+            await client.logout()
+            await client.close()
 
-	if message.author == client.user: #봇이 채팅을 쳤을 때 명령어로 인식되지 않음
-		return
-
-	if message.content == "!안녕":
-		em = discord.Embed(title="안녕",description="안녕",url ="http://google.com", colour=0xDEADBF)
-		em.add_field(name="안녕",value="[안녕](http://google.com)")
-		await client.send_message(message.channel, embed=em)
-
-	if message.content.startswith("!음악"): #음성채널에 봇을 추가 및 음악 재생
-		msg = message.content.split(" ")
-		try:
-			url = msg[1]
-			url1 = re.match('http(?:s?):\/\/(?:www\.)?youtu(?:be\.com\/watch\?v=|\.be\/)([\w\-\_]*)(&(amp;)[\w\=]*)?', url) #정규 표현식을 사용해 url 검사
-			if url1 == None:
-				await client.send_message(message.channel, embed=discord.Embed(title=":no_entry_sign: url을 제대로 입력해주세요.",colour = 0x2EFEF7))
-				return
-		except IndexError:
-			await client.send_message(message.channel, embed=discord.Embed(title=":no_entry_sign: url을 입력해주세요.",colour = 0x2EFEF7))
-			return
-
-		channel = message.author.voice.voice_channel 
-		server = message.server
-		voice_client = client.voice_client_in(server)
-		
-		try:
-			player = await voice_client.create_ytdl_player(url,after=lambda:queue(server.id),before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5")
-			playerlist[server.id] = player
-			playlist.append(player.title)
-		except youtube_dl.utils.DownloadError: #유저가 제대로 된 유튜브 경로를 입력하지 않았을 때
-			await client.send_message(message.channel, embed=discord.Embed(title=":no_entry_sign: 존재하지 않는 경로입니다.",colour = 0x2EFEF7))
-			await voice_client.disconnect()
-			return
-		player.start()
-
-		if client.is_voice_connected(server) and not playerlist[server.id].is_playing(): #봇이 음성채널에 접속해있으나 음악을 재생하지 않을 때
-			await voice_client.disconnect()
-		elif client.is_voice_connected(server) and playerlist[server.id].is_playing(): #봇이 음성채널에 접속해있고 음악을 재생할 때
-			player = await voice_client.create_ytdl_player(url,after=lambda:queue(server.id),before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5")
-			if server.id in que: #큐에 값이 들어있을 때
-				que[server.id].append(player)
-			else: #큐에 값이 없을 때
-				que[server.id] = [player]
-			await client.send_message(message.channel, embed=discord.Embed(title=":white_check_mark: 추가 완료!",colour = 0x2EFEF7))
-			playlist.append(player.title) #재생목록에 제목 추가
-			return
-
-		try:
-			voice_client = await client.join_voice_channel(channel)
-		except discord.errors.InvalidArgument: #유저가 음성채널에 접속해있지 않을 때
-			await client.send_message(message.channel, embed=discord.Embed(title=":no_entry_sign: 음성채널에 접속하고 사용해주세요.",colour = 0x2EFEF7))
-			return
-
-
-	if message.content == "!종료": #음성채널에서 봇을 나가게 하기
-		server = message.server
-		voice_client = client.voice_client_in(server)
-
-		if voice_client == None: #봇이 음성채널에 접속해있지 않았을 때
-			await client.send_message(message.channel, embed=discord.Embed(title=":no_entry_sign: 봇이 음성채널에 없어요.",colour = 0x2EFEF7))
-			return
-		
-		await client.send_message(message.channel, embed=discord.Embed(title=":mute: 채널에서 나갑니다.",colour = 0x2EFEF7)) #봇이 음성채널에 접속해있을 때
-		await voice_client.disconnect()
-
-	if message.content == "!스킵":
-		id = message.server.id
-		if not playerlist[id].is_playing(): #재생 중인 음악이 없을 때
-			await client.send_message(message.channel, embed=discord.Embed(title=":no_entry_sign: 스킵할 음악이 없어요.",colour = 0x2EFEF7))
-			return
-		await client.send_message(message.channel, embed=discord.Embed(title=":mute: 스킵했어요.",colour = 0x2EFEF7))
-		playerlist[id].stop()
+        # Allows owner to set the game status of the bot
+        elif message.content.startswith('status') and message.author.id == config.OWNERID:
+            await client.change_presence(game=discord.Game(name=message.content[7:]))
+	#ex)!status with discord studing > discord studing으로 봇 상태 변경
 	
-	if message.content == "!목록":
-
-		if playlist == []:
-			await client.send_message(message.channel, embed=discord.Embed(title=":no_entry_sign: 재생목록이 없습니다.",colour = 0x2EFEF7))
-			return
-
-		playstr = "```css\n[재생목록]\n\n"
-		for i in range(0, len(playlist)):
-			playstr += str(i+1)+" : "+playlist[i]+"\n"
-		await client.send_message(message.channel, playstr+"```")
-
+	 # Help Message, sends a personal message with a list of all the commands
+        # and how to use them correctly
+        elif message.content.startswith('help'):
+            await client.send_message(message.channel, '1대1로 사용 명령어 보낼게요!(아직 안되는 기능 많아요..)')
+            await client.send_message(message.author, helpMessage.helpMessage)
+	#예시 !help 명령어 입력한 유저에게 features/helpmessage 내용 전달
+	
+      # Searches the second word following pythonhelp in python docs
+        elif message.content.startswith('pythonhelp'):
+            messagetext = message.content
+            split = messagetext.split(' ')
+            if len(split) > 1:
+                messagetext = split[1]
+                await client.send_message(message.channel, 'https://docs.python.org/3/search.html?q=' + messagetext)
+		#pythonhelp 도움말 보냄
+		
+	  # Random cat gif
+        elif message.content.startswith('catgif'):
+            await client.send_message(message.channel, cats.getCatGif())
+	# Random cat picture
+        elif message.content.startswith('cat'):
+            await client.send_message(message.channel, cats.getCatPicture())
+	#features/cats.py
         
 client.run(config.TOKEN)
