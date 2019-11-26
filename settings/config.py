5a9c2daf1605c5bce93e16b1b751acceb2f32f9b
leeahwi <48510004+leeahwi@users.noreply.github.com>
2019-11-26 19:41:16 +0900

    Update config.py

--- settings/config.py
+++ settings/config.py
@@ -3,8 +3,8 @@ import os
 TOKEN = os.environ['BOT_TOKEN']
 KEY = os.environ['BOT_KEY']
 
-BOTID = os.environ['BOT_USER_ID_HERE']
-OWNERID = os.environ['SERVER_OWNER_USER_ID_HERE']
+BOTID = os.environ['BOT_USER_ID']
+OWNERID = os.environ['SERVER_OWNER_USER_ID']
 
 COMMANDPREFIX = '>'
 
